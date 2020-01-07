b938e14ce825859cfd35b74c583e59eb1ca8558a
Iarlyson <Iarlyson.Santana@outlook.com>
2020-01-07 00:19:10 -0200

    Organizando Pastas

---
+++ firabse/firebase.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA5GKLFN8tsFmG2ZmbRaAH_xZUNu5QMjCg",
+    authDomain: "acam-projeto-pds.firebaseapp.com",
+    databaseURL: "https://acam-projeto-pds.firebaseio.com",
+    projectId: "acam-projeto-pds",
+    storageBucket: "acam-projeto-pds.appspot.com",
+    messagingSenderId: "308350155841",
+    appId: "1:308350155841:web:4bc0a31e88b802514b5ac0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
