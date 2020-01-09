e5eb9328a0056cbf226b8a1f358ebb51697effec
Iarlyson <Iarlyson.Santana@outlook.com>
2020-01-08 23:13:14 -0200

    Cadastro 70%

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,13 +1,14 @@
-function CadastroADM(){
-
+function CadastroPaciente(){
 
   var main = document.getElementById("cadastro");
 
   var resultado="<div class=\"caixaCadastro\">";
   resultado+="<form action=\"Pagina-Cadastro\" method=\"post\">";
-  resultado+="<div id=\"caixaTitulo\"><h1 class=\"tituloCadastros\">Cadastro<br>Cadastro Administrador</br></h1>";
+  resultado+="<div id=\"caixaTitulo\"><h1 class=\"tituloCadastros\">Cadastro Paciente</br></h1>";
   resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"Nome:\" class=\"Cadastro\"></div>";
+  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"Data de Nascimento:\": class=\"Cadastro\"></div>";
   resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"CPF\": class=\"Cadastro\"></div>";
+  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"SUS:\" class=\"Cadastro\"></div>";
   resultado+="<div id=\"camposCadastro\"><input type=\"email\" placeholder=\"E-mail:\" class=\"Cadastro\"></div>";
   resultado+="<div id=\"camposCadastro\"><input type=\"password\" placeholder=\"Senha:\" class=\"Cadastro\"></div>";
   resultado+="<div id=\"criarConta\"><input type=\"submit\" value=\"Criar conta\" class=\"botãoCadastro\"></div>";
@@ -15,25 +16,7 @@ function CadastroADM(){
   main.innerHTML = resultado;
 }
 
-function CadastroAtendente(){
 
-  var main = document.getElementById("cadastro");
-
-  var resultado="<div class=\"caixaCadastro\">";
-  resultado+="<form action=\"Pagina-Cadastro\" method=\"post\">";
-  resultado+="<div id=\"caixaTitulo\"><h1 class=\"tituloCadastros\">Cadastro Atendente</br></h1>";
-  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"Nome:\" class=\"Cadastro\"></div>";
-  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"CPF\": class=\"Cadastro\"></div>";
-  resultado+="<div id=\"camposCadastro\"><input type=\"email\" placeholder=\"E-mail:\" class=\"Cadastro\"></div>";
-  resultado+="<div id=\"camposCadastro\"><input type=\"password\" placeholder=\"Senha:\" class=\"Cadastro\"></div>";
-  resultado+="<div id=\"criarConta\"><input type=\"submit\" value=\"Criar conta\" class=\"botãoCadastro\"></div>";
-  resultado+="</form></div>"
-  main.innerHTML = resultado;
-  
-}
-
-
- 
 function CadastroMedico(){
 
   var main = document.getElementById("cadastro");
@@ -53,21 +36,42 @@ function CadastroMedico(){
 
 }
 
-function CadastroPaciente(){
+function CadastroAtendente(){
 
   var main = document.getElementById("cadastro");
 
   var resultado="<div class=\"caixaCadastro\">";
   resultado+="<form action=\"Pagina-Cadastro\" method=\"post\">";
-  resultado+="<div id=\"caixaTitulo\"><h1 class=\"tituloCadastros\">Cadastro Médico</br></h1>";
+  resultado+="<div id=\"caixaTitulo\"><h1 class=\"tituloCadastros\">Cadastro Atendente</br></h1>";
   resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"Nome:\" class=\"Cadastro\"></div>";
-  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"Data de Nascimento:\": class=\"Cadastro\"></div>";
   resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"CPF\": class=\"Cadastro\"></div>";
-  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"SUS:\" class=\"Cadastro\"></div>";
   resultado+="<div id=\"camposCadastro\"><input type=\"email\" placeholder=\"E-mail:\" class=\"Cadastro\"></div>";
   resultado+="<div id=\"camposCadastro\"><input type=\"password\" placeholder=\"Senha:\" class=\"Cadastro\"></div>";
   resultado+="<div id=\"criarConta\"><input type=\"submit\" value=\"Criar conta\" class=\"botãoCadastro\"></div>";
   resultado+="</form></div>"
   main.innerHTML = resultado;
-  console.log("Paciente")
+  
 }
+
+function CadastroADM(){
+
+
+  var main = document.getElementById("cadastro");
+
+  var resultado="<div class=\"caixaCadastro\">";
+  resultado+="<form action=\"Pagina-Cadastro\" method=\"post\">";
+  resultado+="<div id=\"caixaTitulo\"><h1 class=\"tituloCadastros\">Cadastro Administrador</br></h1>";
+  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"Nome:\" class=\"Cadastro\"></div>";
+  resultado+="<div id=\"camposCadastro\"><input type=\"text\" placeholder=\"CPF\": class=\"Cadastro\"></div>";
+  resultado+="<div id=\"camposCadastro\"><input type=\"email\" placeholder=\"E-mail:\" class=\"Cadastro\"></div>";
+  resultado+="<div id=\"camposCadastro\"><input type=\"password\" placeholder=\"Senha:\" class=\"Cadastro\"></div>";
+  resultado+="<div id=\"criarConta\"><input type=\"submit\" value=\"Criar conta\" class=\"botãoCadastro\"></div>";
+  resultado+="</form></div>"
+  main.innerHTML = resultado;
+}
+
+
+
+ 
+
+
